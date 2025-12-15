714f69cafe4a17575cf6e37b034a496276a5eaa4
Alok Jha <alokkumarjha773762@gmail.com>
2025-12-15 23:02:50 +0530

    createsessionRequest modified  1.1

--- src/main/java/com/secretsanta/backend/dto/CreateSessionRequest.java
+++ src/main/java/com/secretsanta/backend/dto/CreateSessionRequest.java
@@ -1,9 +1,18 @@
 package com.secretsanta.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateSessionRequest {
     private List<String> members;
 }
